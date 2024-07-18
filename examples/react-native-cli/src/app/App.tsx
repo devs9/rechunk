d31728e71da8f0786dac8a70cda7c9b487a863cf
devs9 <devs9gp@gmail.com>
2024-07-18 22:28:39 +0200

    feat(examples): create mock file for rn-cli

--- examples/react-native-cli/src/app/App.tsx
+++ examples/react-native-cli/src/app/App.tsx
@@ -3,6 +3,7 @@ import {ErrorBoundary} from 'react-error-boundary';
 import {ActivityIndicator, StyleSheet, SafeAreaView, View} from 'react-native';
 import {importChunk} from '@crherman7/rechunk';
 import {Error404} from '@/components';
+import {balanceData, transactionsData} from '@/lib';
 
 const Card = lazy(() => importChunk('card'));
 const Balance = lazy(() => importChunk('balance'));
@@ -23,74 +24,6 @@ export default function App() {
     </ErrorBoundary>
   );
 }
-
-const balanceData = {
-  total: 921.27,
-  data: {
-    labels: ['test'],
-    datasets: [
-      {
-        data: [
-          0,
-          25 + Math.random() * 25,
-          50 + Math.random() * 25,
-          75 + Math.random() * 25,
-          75 + Math.random() * 25,
-          100 + Math.random() * 25,
-          100 + Math.random() * 25,
-          120 + Math.random() * 25,
-          130 + Math.random() * 25,
-          150 + Math.random() * 25,
-        ],
-      },
-    ],
-  },
-};
-
-const transactionsData = [
-  {
-    title: 'Akamai Coffee Shop',
-    location: 'Kihei, HI',
-    date: 'Today, 13:21',
-    amount: (Math.random() * 10).toFixed(2),
-    icon: '#FB8E41',
-  },
-  {
-    title: 'Shops at Wailea',
-    location: 'Wailea, HI',
-    date: 'Yesterday, 20:07',
-    amount: (Math.random() * 25).toFixed(2),
-    icon: '#0091FF',
-  },
-  {
-    title: 'Ono Hawaiian BBQ',
-    location: 'Paia, HI',
-    date: 'Thursday',
-    amount: (Math.random() * 100).toFixed(2),
-    icon: '#34D058',
-  },
-  {
-    title: 'Fond',
-    location: 'Lahaina, HI',
-    date: 'Wensday',
-    amount: (Math.random() * 10).toFixed(2),
-    icon: '#34D058',
-  },
-  {
-    title: 'Ula’Ula Cafe',
-    location: 'Waihee-Waiehu, HI',
-    date: 'Tuesday',
-    amount: (Math.random() * 15).toFixed(2),
-    icon: '#FB8E41',
-  },
-  {
-    title: "Tante's Fishmarket",
-    location: 'Wailuku, HI',
-    date: 'Monday',
-    amount: (Math.random() * 10).toFixed(2),
-    icon: '#0091FF',
-  },
-];
 
 const styles = StyleSheet.create({
   container: {
