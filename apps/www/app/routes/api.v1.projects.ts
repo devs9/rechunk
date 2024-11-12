e516681622373d44667a40025d2334d6223d43ff
Christopher Herman <crherman7@gmail.com>
2024-11-12 17:17:34 -0500

    feat: api implementation (#52)
    * feat: api implementation
    
    * fix: json schema adherence
    
    * chore(deps): remove server app
    
    * fix(deps): missing tsup

---
+++ apps/www/app/routes/api.v1.projects.ts
@@ -0,0 +1,32 @@
+import type {ActionFunction} from '@remix-run/node';
+import {json} from '@remix-run/node';
+
+import {requireBasicAuth} from '~/utils/auth';
+import {handleError} from '~/utils/error';
+import {createProject} from '~/models/project.server';
+
+export const action: ActionFunction = async ({request}) => {
+  try {
+    await requireBasicAuth(request);
+
+    // Assuming the project data is sent in the request body
+    const {id, readKey, writeKey, publicKey, privateKey} =
+      await createProject();
+
+    return json(
+      {
+        $schema: 'https://crherman7.github.io/rechunk/schema.json',
+        project: id,
+        readKey,
+        writeKey,
+        publicKey,
+        privateKey,
+        entry: {},
+        external: [],
+      },
+      {status: 200},
+    );
+  } catch (error) {
+    return handleError(error);
+  }
+};
